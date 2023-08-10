5736d52c938613d14fbb806a40d2e3251c529ec8
Francisco Ramos Bisneto <framos.df@gmail.com>
2023-08-10 11:04:14 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/fran/descomplica/filmeflix/FilmeFlixApplication.java
@@ -0,0 +1,13 @@
+package br.com.fran.descomplica.filmeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeFlixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmeFlixApplication.class, args);
+    }
+
+}
