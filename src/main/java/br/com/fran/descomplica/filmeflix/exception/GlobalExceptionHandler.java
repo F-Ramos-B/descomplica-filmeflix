a7463129e9ebe3bfdc59bcf67462b20345a01cbc
Francisco Ramos Bisneto <framos.df@gmail.com>
2023-08-13 01:09:52 -0300

    Ajuste tratamento de erros

--- src/main/java/br/com/fran/descomplica/filmeflix/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/fran/descomplica/filmeflix/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.fran.descomplica.filmeflix.exception;
 
 import br.com.fran.descomplica.filmeflix.dto.ErroDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -10,20 +11,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
+    
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErroDTO> tratarException(Exception ex) {
         return gerarErroDTO(ex, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
+    
     @ExceptionHandler({AccessDeniedException.class, BadCredentialsException.class})
     protected ResponseEntity<ErroDTO> tratarAuthException(Exception ex) {
         return gerarErroDTO(ex, HttpStatus.UNAUTHORIZED);
     }
-
+    
     private ResponseEntity<ErroDTO> gerarErroDTO(Exception ex, final HttpStatus error) {
+        log.error("Erro", ex);
         return new ResponseEntity<>(new ErroDTO(ex.getLocalizedMessage(), error.value(), error.getReasonPhrase()), error);
     }
-
+    
 }
