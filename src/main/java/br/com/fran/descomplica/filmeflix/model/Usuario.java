052f0dc07ddde224c1b6adf1fb62027543e1e4e1
Francisco Ramos Bisneto <framos.df@gmail.com>
2023-08-14 15:33:28 -0300

    Ajuste username para email

--- src/main/java/br/com/fran/descomplica/filmeflix/model/Usuario.java
+++ src/main/java/br/com/fran/descomplica/filmeflix/model/Usuario.java
@@ -35,80 +35,80 @@ import org.springframework.security.core.userdetails.UserDetails;
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class Usuario extends BaseEntity implements UserDetails {
-    
+
     @Size(max = 100)
     private String nome;
-    
+
     @Size(max = 30)
     private String apelido;
-    
+
     @Size(max = 100)
     private String senha;
-    
+
     @Size(max = 50)
     private String email;
-    
+
     @Size(max = 50)
     private String genero;
-    
+
     @Size(max = 250)
     @Column(name = "link_avatar")
     private String linkAvatar;
-    
+
     @Column(name = "data_nascimento")
     private LocalDate dataNascimento;
-    
+
     @Column(name = "created_at")
     @CreationTimestamp
     private LocalDateTime createdAt;
-    
+
     @Column(name = "updatedAt")
     @UpdateTimestamp
     private LocalDateTime updatedAt;
-    
+
     @OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
     private List<Contato> contatos = new ArrayList<>();
-    
+
     @Enumerated(EnumType.ORDINAL)
     @Column(name = "id_perfil")
     private EnumPerfil perfil;
-    
+
     @OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
     private List<Avaliacao> avaliacoes = new ArrayList<>();
-    
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.singletonList(new SimpleGrantedAuthority(perfil.toString()));
     }
-    
+
     @Override
     public String getPassword() {
         return senha;
     }
-    
+
     @Override
     public String getUsername() {
-        return apelido;
+        return email;
     }
-    
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-    
+
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-    
+
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-    
+
     @Override
     public boolean isEnabled() {
         return true;
     }
-    
+
 }
